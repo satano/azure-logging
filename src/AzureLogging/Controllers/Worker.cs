dae4d753f36d0366f49d47bfeb1188f6df97ce5c
Stano Peťko <stano@stanopetko.eu>
2022-05-25 12:49:51 +0200

    Initial project

---
+++ src/AzureLogging/Controllers/Worker.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AzureLogging.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Worker : ControllerBase
+    {
+        private static readonly Random _random = new();
+
+        [HttpGet("{name}")]
+        public async Task<string> Do(string name)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            await Task.Delay(_random.Next(2000));
+            sw.Stop();
+
+            return $"Worked on task {name} for {sw.ElapsedMilliseconds} ms.";
+        }
+    }
+}
